1f355612fce54d19c37f3901e24411d4f9bf6836
Sergiy Fomin <fominssergiy@gmail.com>
2021-10-06 09:42:17 -0400

    terraform fmt on provider.tf file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
     datadog = {
-      source = "DataDog/datadog"
+      source  = "DataDog/datadog"
       version = "3.4.0"
     }
   }
